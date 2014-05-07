31f7bcfe72b6834b9d24acbe00fe68b1af79629f
Stefan Dietiker <stefand@ethz.ch>
2014-05-07 10:11:14 +0200

    root is an array of ints

--- src/bst.h
+++ src/bst.h
@@ -1,4 +1,22 @@
 #ifndef BST_H
 #define BST_H
 
+#include <stdint.h>
+
+void* bst_alloc( size_t n );
+
+/**
+ * @brief bst_compute
+ * @param root
+ * @param p
+ * @param q
+ * @param n
+ * @return
+ */
+double bst_compute( int* root, double* p, double* q, size_t n, void*_mem );
+
+double bst_root_get( double* root, size_t i, size_t j );
+
+void bst_free( void* _mem );
+
 #endif // BST_H
