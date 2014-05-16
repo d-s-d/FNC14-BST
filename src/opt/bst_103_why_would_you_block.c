7f1428a4a8ffecf57b8bd2ca00c01504583364f5
Jeremia Bär <jeb@jebdev.net>
2014-05-16 17:57:34 +0200

    Still don't understand why blocking is faster

---
+++ src/opt/bst_103_why_would_you_block.c
@@ -0,0 +1,107 @@
+#include<bst.h>
+#include<math.h>
+#include<string.h>
+
+
+/*
+ * Disclaimer: The indices differ from the high-level pseudo-code description
+ * found in the book [1].
+ *
+ * Here, e[i,j] is the expected value for the tree containing keys i to j-1,
+ * since the first key is p_0 and not p_1 as in the book. w[.,.] and root[.,.]
+ * are defined similarly.
+ *
+ */
+
+/*
+ * This version applies scalar replacement to bst_100_ref_bottomup.
+ */
+
+#define STRIDE (n+1)
+#define IDX(i,j) ((i)*STRIDE + j)
+
+typedef struct {
+    double* e;
+    double* w;
+    int* r;
+    size_t n;
+} segments_t;
+
+void* bst_alloc_103_why_would_you_block( size_t n ) {
+    segments_t* mem = (segments_t*) malloc( sizeof(segments_t) );
+    size_t sz = (n+1)*(n+1);
+    // XXX: for testing: calloc
+    mem->e = (double*) calloc(1,  sz * sizeof(double) );
+    mem->w = (double*) calloc(1,  sz * sizeof(double) );
+    mem->r = (int*)    calloc(1,  sz * sizeof(int) );
+    memset( mem->r, -1, sz * sizeof(int) );
+    return mem;
+}
+
+//#define ACCESS_PRINT(i,j,r) {printf("ACCESS i=%d j=%d r=%d\n", i, j, r);}
+
+double bst_compute_103_why_would_you_block( void*_bst_obj, double* p, double* q, size_t nn ) {
+    segments_t* mem = (segments_t*) _bst_obj;
+    int n;
+    int i, l, r, j;
+    double t, t_min, w_cur;
+    int r_min;
+    double* e = mem->e, *w = mem->w;
+    int* root = mem->r;
+    // initialization
+    mem->n = nn;
+    n = nn; // subtractions with n potentially negative. say hello to all the bugs
+    for( i = 0; i < n+1; i++ ) {
+        e[IDX(i,i)] = q[i];
+        w[IDX(i,i)] = q[i];
+    }
+
+    for (i = n-1; i >= 0; --i) {
+        for (j = i+1; j < n+1; ++j) {
+            e[IDX(i,j)] = INFINITY;
+        }
+
+        for (r=i; r<n; ++r) {
+            for (j = r+1; j < n+1; ++j) {
+                w[IDX(i,j)] = w[IDX(i,j-1)] + p[j-1] + q[j];
+                    t = e[IDX(i,r)] + e[IDX(r+1,j)] + w[IDX(i,j)];
+                    //ACCESS_PRINT(i, j, r);
+                    if (t < e[IDX(i,j)]) {
+                        e[IDX(i,j)] = t;
+                        root[IDX(i,j)] = r;
+                    }
+                }
+        }
+    }
+
+
+    return e[IDX(0,n)];
+}
+
+size_t bst_get_root_103_why_would_you_block( void* _bst_obj, size_t i, size_t j )
+{
+    // [i,j], in table: [i-1, j]+1
+    segments_t *mem = _bst_obj;
+    size_t n = mem->n;
+    int *root = mem->r;
+    return (size_t) root[(i-1)*(n+1)+j]+1;
+}
+
+void bst_free_103_why_would_you_block( void* _mem ) {
+    segments_t* mem = (segments_t*) _mem;
+
+    /*
+    size_t n = mem->n;
+    for (size_t i=0; i<=n; ++i) {
+        for (size_t j=0; j<=n; ++j) {
+            printf(" %.3lf", mem->e[i*(n+1)+j]);
+        }
+        printf("\n");
+    }
+    */
+
+    free( mem->e );
+    free( mem->w );
+    free( mem->r );
+    free( mem );
+}
